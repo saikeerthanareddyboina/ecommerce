ea6edd99514de73037ea60e92bcd96fa60f34a97
ajay <ajaygorripotu673@gmail.com>
2025-12-26 15:30:48 +0530

    ajay1

---
+++ pricepridiction/keajna/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KeajnaConfig(AppConfig):
+    name = 'keajna'
---
+++ pricepridiction/pricepridiction/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pricepridiction project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricepridiction.settings')
+
+application = get_asgi_application()
